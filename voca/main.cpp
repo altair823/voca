e0f805ae6585a3995d2bc28b4379137781f85b73
altair823 <dlsrks0734@gmail.com>
2020-10-24 05:43:56 +0900

    Initial Commit

---
+++ voca/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  voca
+//
+//  Created by 김태현 on 2020/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
